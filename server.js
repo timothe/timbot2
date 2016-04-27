e3b92ee544b4d0664a6064059bc365a00b4073d1
Tim Leroy <timothe.leroy@gmail.com>
2016-04-27 11:37:49 +0200

    Fixed conversations

--- server.js
+++ server.js
@@ -57,83 +57,102 @@ controller.hears(['his life', 'tim\'s life', 'his universe'], 'message_received'
 });
 
 controller.hears(['his experience', 'tim\'s experience'], 'message_received', function (bot, message) {
-	bot.reply(message, 'He worked 8 years in Paris in agencies and 2 years in London in tech companies.')
-	bot.reply(message, {
-		attachment: {
-		  type: 'template',
-		  payload: {
-		    template_type: 'button',
-		    text: 'Do you want to know more about his experience in...',
-		    buttons: [
-		      {
-		        type: 'postback',
-		        title: 'London',
-		        payload: 'show_london'
-		      },
-		      {
-		        type: 'postback',
-		        title: 'Paris',
-		        payload: 'show_paris'
-		      }
-		    ]
-		  }
-		}
+	bot.startConversation(message,function(err,convo) {	
+		convo.say('He worked 8 years in Paris in agencies and 2 years in London in tech companies')
+		convo.say(message, {
+			attachment: {
+			  type: 'template',
+			  payload: {
+			    template_type: 'button',
+			    text: 'Do you want to know more about his experience in...',
+			    buttons: [
+			      {
+			        type: 'postback',
+			        title: 'London',
+			        payload: 'show_london'
+			      },
+			      {
+			        type: 'postback',
+			        title: 'Paris',
+			        payload: 'show_paris'
+			      }
+			    ]
+			  }
+			}
+		})
 	})
-});
+})
 
 controller.hears(['ogilvy'], 'message_received', function (bot, message) {
-	bot.reply(message, 'MRM Worldwide is part of the McCann Erickson ad agencies group. This division is specialized in CRM communication with Nestlé, Gemey and Opel as their key clients')
-	bot.reply(message, 'Tim was a Project Manager there, working on Care, Mastercard, Gaz de France, and Eurofactor')
+	bot.startConversation(message,function(err,convo) {	
+		convo.say('MRM Worldwide is part of the McCann Erickson ad agencies group. This division is specialized in CRM communication with Nestlé, Gemey and Opel as their key clients');
+		convo.say('Tim was a Project Manager there, working on Care, Mastercard, Gaz de France, and Eurofactor');
+	});
 });
 
 controller.hears(['tbwa'], 'message_received', function (bot, message) {
-	bot.reply(message, 'TBWA interactive is part of the TBWA ad agencies group. This division is specialized in digital communication')
-	bot.reply(message, 'On his 1st year Tim was focusing on Project Management, then he asked to switch to Technical Project Management')
-	bot.reply(message, 'He did a lot of different things there: concept design, development, technical consulting')
-	bot.reply(message, '... and worked on great clients: ADIDAS, Arcelor Mittal, BNP Paribas, Michelin, McDonald’s, Amnesty, SNCF...')
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'TBWA interactive is part of the TBWA ad agencies group. This division is specialized in digital communication')
+		convo.say(message, 'On his 1st year Tim was focusing on Project Management, then he asked to switch to Technical Project Management')
+		convo.say(message, 'He did a lot of different things there: concept design, development, technical consulting')
+		convo.say(message, '... and worked on great clients: ADIDAS, Arcelor Mittal, BNP Paribas, Michelin, McDonald’s, Amnesty, SNCF...')
+	});
+
 });
 
 controller.hears(['milky'], 'message_received', function (bot, message) {
-	bot.reply(message, 'Milky is a web agency leading brands to perform successful online communication')
-	bot.reply(message, 'The great Milky adventure began in 2009 with 3 other business partners. Tim was the Head of project management, also doing Business development and People Management')
-	bot.reply(message, 'The key clients were: ADIDAS, L’Oréal, Bourjois (FR, UK, CA), Etam, Longchamp, Nivea, Sarenza, Warner Bros, EMI, Voyages SNCF')
-	bot.reply(message, 'A company was interested in buying the agency in 2011. Tim decided to sell and go, while the partners and employees stayed')
-	bot.reply(message, 'Tim created Mustang a short time after')
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'Milky is a web agency leading brands to perform successful online communication')
+		convo.say(message, 'The great Milky adventure began in 2009 with 3 other business partners. Tim was the Head of project management, also doing Business development and People Management')
+		convo.say(message, 'The key clients were: ADIDAS, L’Oréal, Bourjois (FR, UK, CA), Etam, Longchamp, Nivea, Sarenza, Warner Bros, EMI, Voyages SNCF')
+		convo.say(message, 'A company was interested in buying the agency in 2011. Tim decided to sell and go, while the partners and employees stayed')
+		convo.say(message, 'Tim created Mustang a short time after')
+	});
 });
 
 controller.hears(['mustang'], 'message_received', function (bot, message) {
-	bot.reply(message, 'After selling his Milky shares, Tim immediately created Mustang Interactive, another web developement agency')
-	bot.reply(message, 'Some clients followed him in the deal, and the company grew \'till 2014, when Tim decided to close it and move to London')
-});
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'After selling his Milky shares, Tim immediately created Mustang Interactive, another web developement agency')
+		convo.say(message, 'Some clients followed him in the deal, and the company grew \'till 2014, when Tim decided to close it and move to London')	
+	})
+})
 
 controller.hears(['criteo'], 'message_received', function (bot, message) {
-	bot.reply(message, 'Criteo helps advertisers generate more sales through personalized performance advertising (retargeting) at a global scale')
-	bot.reply(message, 'Tim had a very busy time there, where he was doing the onboarding for new clients, follow-up to debug and optimise existing campaigns...')
-	bot.reply(message, 'review and interview potential TS candidates')
-	bot.reply(message, 'train newcomers')
-	bot.reply(message, 'He worked on Desktop, Mobile web, In-App and Email advertising but also on publishers’ networks using different technologies (RTA, RTB, Passback, Cookie targeting)')
-	bot.reply(message, 'Tim got in touch on a day-to-day basis with the worldwide teams of Marks & Spencer, John Lewis, Debenhams, House of Fraser, Gap / Banana Republic, Hotels.com, Expedia, Hostel World, HomeAway, Monarch, Gumtree, Kijiji, EON, ...')
-});
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'Criteo helps advertisers generate more sales through personalized performance advertising (retargeting) at a global scale')
+		convo.say(message, 'Tim had a very busy time there, where he was doing the onboarding for new clients, follow-up to debug and optimise existing campaigns...')
+		convo.say(message, 'review and interview potential TS candidates')
+		convo.say(message, 'train newcomers')
+		convo.say(message, 'He worked on Desktop, Mobile web, In-App and Email advertising but also on publishers’ networks using different technologies (RTA, RTB, Passback, Cookie targeting)')
+		convo.say(message, 'Tim got in touch on a day-to-day basis with the worldwide teams of Marks & Spencer, John Lewis, Debenhams, House of Fraser, Gap / Banana Republic, Hotels.com, Expedia, Hostel World, HomeAway, Monarch, Gumtree, Kijiji, EON, ...')
+	})
+})
 
 controller.hears(['smartfocus'], 'message_received', function (bot, message) {
-	bot.reply(message, 'SmartFocus enhances digital strategies for businesses with The Message Cloud, a powerful platform featuring big data-fuelled insights and enabling smart multi- channel campaigns')
-	bot.reply(message, 'Tim is pretty new there but he already met all the expectations of this different company and job')
-	bot.reply(message, 'His role consists of teaming up with sales people and meeting clients in Europe to demonstrate the abilities of the platform, create demo environments and dummy websites to contextualize the pitch according to the client’s needs')
-	bot.reply(message, 'But also answer RFPs / RFIs with the help of the Professional Services team and support the new Presales team by building a knowledge base and core materials')
-});
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'SmartFocus enhances digital strategies for businesses with The Message Cloud, a powerful platform featuring big data-fuelled insights and enabling smart multi- channel campaigns')
+		convo.say(message, 'Tim is pretty new there but he already met all the expectations of this different company and job')
+		convo.say(message, 'His role consists of teaming up with sales people and meeting clients in Europe to demonstrate the abilities of the platform, create demo environments and dummy websites to contextualize the pitch according to the client’s needs')
+		convo.say(message, 'But also answer RFPs / RFIs with the help of the Professional Services team and support the new Presales team by building a knowledge base and core materials')
+	})
+})
 
 controller.hears(['education', 'school', 'masters', 'degree'], 'message_received', function (bot, message) {
-	bot.reply(message, 'Tim studied at the University of Toulon (France) and graduated with a Master’s Degree \"Internet and Multimedia Engineering\" in 2006')
-	bot.reply(message, 'This curriculum is proposing a variety of classes for the professions around internet: Development, Design, Marketing, Law, etc. but also filming: Writing, Photography, Storyboard, Montage, etc')
-	bot.reply(message, 'The objective is to become a skilful Project Manager')
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'Tim studied at the University of Toulon (France) and graduated with a Master’s Degree \"Internet and Multimedia Engineering\" in 2006')
+		convo.say(message, 'This curriculum is proposing a variety of classes for the professions around internet: Development, Design, Marketing, Law, etc. but also filming: Writing, Photography, Storyboard, Montage, etc')
+		convo.say(message, 'The objective is to become a skilful Project Manager')
+	});
 })
 
 controller.hears(['hobbies'], 'message_received', function (bot, message) {
-	bot.reply(message, 'Tim is a fan of Internet culture in general, technology, and science')
-	bot.reply(message, 'He spends loads of time watching (qualitative) TV series and organising his next trip abroad')
-	bot.reply(message, 'Sports-wise, he\'s regularly practicing Muay Thai after years of the a classic jogging / weight lifting routine')
-	bot.reply(message, 'He\'s a big fan of cars and motorbikes too and got the chance to participate in some races in the past')
-	bot.reply(message, 'Well, among other cool stories ;)')
+	bot.startConversation(message,function(err,convo) {	
+		convo.say(message, 'Tim is a fan of Internet culture in general, technology, and science')
+		convo.say(message, 'He spends loads of time watching (qualitative) TV series and organising his next trip abroad')
+		convo.say(message, 'Sports-wise, he\'s regularly practicing Muay Thai after years of the a classic jogging / weight lifting routine')
+		convo.say(message, 'He\'s a big fan of cars and motorbikes too and got the chance to participate in some races in the past')
+		convo.say(message, 'Well, among other cool stories ;)')
+	});
 })
 
 controller.hears(['emea'], 'message_received', function (bot, message) {
@@ -153,7 +172,7 @@ controller.hears(['suck'], 'message_received', function (bot, message) {
 })
 
 controller.hears(['fuck', 'twat', 'cunt', 'bastard', 'dick', 'pute', 'putain'], 'message_received', function (bot, message) {
-	bot.reply(message, '😒')
+	bot.reply(message, '-_-')
 })
 
 controller.hears(['francais'], 'message_received', function (bot, message) {
@@ -164,30 +183,34 @@ controller.hears(['francais'], 'message_received', function (bot, message) {
 controller.on('facebook_postback', function (bot, message) {
   switch (message.payload) {
     case 'show_london':
-      bot.reply(message, {
-        attachment: {
-          type: 'image',
-          payload: {
-            url: 'http://thecoffeevine.com/wp-content/uploads/2016/04/london.jpg'
-          }
-        }
-      })
-      bot.reply(message, 'He was hired by Criteo in 2014 as a Senior Technical Solutions Engineer. In 2016 he left the company to be hired at SmartFocus as a Presales Consultant')
-      break
+		bot.startConversation(message,function(err,convo) {	
+		  convo.say(message, {
+		    attachment: {
+		      type: 'image',
+		      payload: {
+		        url: 'http://thecoffeevine.com/wp-content/uploads/2016/04/london.jpg'
+		      }
+		    }
+		  })
+		  convo.say(message, 'He was hired by Criteo in 2014 as a Senior Technical Solutions Engineer. In 2016 he left the company to be hired at SmartFocus as a Presales Consultant')
+		});
+        break
     case 'show_paris':
-      bot.reply(message, {
-        attachment: {
-          type: 'image',
-          payload: {
-            url: 'https://c8.staticflickr.com/8/7003/6672156239_01bde2b717_b.jpg'
-          }
-        }
-      })
-      bot.reply(message, 'Tim began his career in 2006 with 6 months of internship as a project manager at MRM Worldwide')
-      bot.reply(message, '...then at TBWA from 2006 to 2008 as a Project Manager and Technical Project Manager')
-      bot.reply(message, '...and ~1 year at Ogilvy again as a Technical Project Manager.')
-      bot.reply(message, 'He then became an entrepreneur and built Milky at the end of 2009. The company was bought in 2011, he left the project and built Mustang, which he ran \'till 2014')
-      break
+		bot.startConversation(message,function(err,convo) {	
+		  convo.say(message, {
+		    attachment: {
+		      type: 'image',
+		      payload: {
+		        url: 'https://c8.staticflickr.com/8/7003/6672156239_01bde2b717_b.jpg'
+		      }
+		    }
+		  })
+		  convo.say(message, 'Tim began his career in 2006 with 6 months of internship as a project manager at MRM Worldwide')
+		  convo.say(message, '...then at TBWA from 2006 to 2008 as a Project Manager and Technical Project Manager')
+		  convo.say(message, '...and ~1 year at Ogilvy again as a Technical Project Manager.')
+		  convo.say(message, 'He then became an entrepreneur and built Milky at the end of 2009. The company was bought in 2011, he left the project and built Mustang, which he ran \'till 2014')
+		});
+        break
   }
 });
 
@@ -278,16 +301,16 @@ controller.hears(['shutdown'], 'message_received', function(bot, message) {
     bot.startConversation(message, function(err, convo) {
 
         convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
+        {
+            pattern: bot.utterances.yes,
+            callback: function(response, convo) {
+                convo.say('Bye!');
+                convo.next();
+                setTimeout(function() {
+                    process.exit();
+                }, 3000);
+            }
+        },
         {
             pattern: bot.utterances.no,
             default: true,
@@ -301,15 +324,11 @@ controller.hears(['shutdown'], 'message_received', function(bot, message) {
 });
 
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'message_received',
-    function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':|] I am a bot named Timbot. I have been running for ' + uptime + '.');
-    });
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'message_received', function(bot, message) {
+    var hostname = os.hostname();
+    var uptime = formatUptime(process.uptime());
+    bot.reply(message, ':|] I am a bot named Timbot. I have been running for ' + uptime + '.');
+});
 
 
 
@@ -333,6 +352,6 @@ function formatUptime(uptime) {
         unit = unit + 's';
     }
 
-    uptime = uptime + ' ' + unit;
+    uptime = uptime.toFixed(2) + ' ' + unit;
     return uptime;
 }
